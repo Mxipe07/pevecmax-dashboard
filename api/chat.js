a46c8b9a3b7b2d429672fa72c2bc92b788d89ac1
Mxipe07 <pevecmax07@gmail.com>
2025-10-30 07:04:11 +0100

    Delete api directory

--- api/chat.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
-  const { message } = await req.json();
-  const reply = `(${new Date().toLocaleTimeString()}) Echo: ${message}`;
-  return new Response(JSON.stringify({ reply }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}

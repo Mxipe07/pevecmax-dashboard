9b4d55205be4fef2d975423b6cc805194a6dff2f
Mxipe07 <pevecmax07@gmail.com>
2025-10-31 00:57:19 +0100

    Delete api/auth directory

--- api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Set-Cookie', [
-    'pmx_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0'
-  ]);
-  res.writeHead(302, { Location: '/' });
-  res.end();
-}
